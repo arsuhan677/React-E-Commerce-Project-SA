226e65d4309673d83c6613d114b94a62a661b84b
Suhan <arsuhan677@gmail.com>
2025-12-02 00:26:56 +0600

    update for about pages

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -7,16 +7,16 @@ const Products = () => {
   const [query, setQuery] = useState("");
 
   const products = [
-    { id: 1, name: "Winter Jacket", price: "25", image: "./public/images1/1.avif" },
-    { id: 2, name: "KeyBoard", price: "30", image: "./public/images1/2.avif" },
-    { id: 3, name: "T-Shirt", price: "35", image: "./public/images1/3.avif" },
-    { id: 4, name: "Leptop HP", price: "40", image: "./public/images1/4.avif" },
-    { id: 5, name: "T-Shirt", price: "45", image: "./public/images1/5.avif" },
-    { id: 6, name: "Winter Jacket", price: "50", image: "./public/images1/6.avif" },
-    { id: 7, name: "T-Shirt", price: "60", image: "./public/images1/7.avif" },
-    { id: 8, name: "Winter Jacket", price: "20", image: "./public/images1/8.avif" },
-    { id: 9, name: "T-Shirt", price: "50", image: "./public/images1/10.avif" },
-    { id: 10, name: "Winter Jacket", price: "20", image: "./public/images1/9.avif" },
+    { id: 1, name: "Winter Jacket", price: "25", image: "./images1/1.avif" },
+    { id: 2, name: "KeyBoard", price: "30", image: "./images1/2.avif" },
+    { id: 3, name: "T-Shirt", price: "35", image: "./images1/3.avif" },
+    { id: 4, name: "Leptop HP", price: "40", image: "./images1/4.avif" },
+    { id: 5, name: "T-Shirt", price: "45", image: "./images1/5.avif" },
+    { id: 6, name: "Winter Jacket", price: "50", image: "./images1/6.avif" },
+    { id: 7, name: "T-Shirt", price: "60", image: "./images1/7.avif" },
+    { id: 8, name: "Winter Jacket", price: "20", image: "./images1/8.avif" },
+    { id: 9, name: "T-Shirt", price: "50", image: "./images1/10.avif" },
+    { id: 10, name: "Winter Jacket", price: "20", image: "./images1/9.avif" },
   ];
 
   const filteredProducts = products.filter((p) =>
